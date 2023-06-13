b56fe71d442d4e2452d3ce4010b3ace2180518f9
Farman Malik <farmanmalik4487@gmail.com>
2023-06-13 17:32:21 +0530

    First commit

---
+++ ReactPieChart/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import React from "react";
+import PieChart from "./component/PieChart";
+function App() {
+  return (
+    <div className="App">
+      <div className="pie-div">
+      <PieChart  />
+
+      </div>
+    </div>
+  )
+}
+
+export default App;
